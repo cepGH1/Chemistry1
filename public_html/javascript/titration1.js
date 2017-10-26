5c8c4d5d27b4755bb9e7faa72c8a12a7ca1a9098
clare <clarepritchard@hotmail.com>
2017-10-26 11:09:28 +0100

    Add files via upload

---
+++ public_html/javascript/titration1.js
@@ -0,0 +1,24 @@
+/* 
+ * CEP 2017
+ * GCSE Chemistry
+ */
+
+function wordEquations(){
+     var myWindow = window.open("wordEquations.html", "myWindow", "width=800,height=500");
+}
+function formulaEquations(){
+     var myWindow = window.open("formulaEquations.html", "myWindow", "width=800,height=500");
+}
+function molarEquivalences(){
+     var myWindow = window.open("molarEquivalence.html", "myWindow", "width=800,height=500");
+}
+function concentrations(){
+     var myWindow = window.open("./definitions/concentration.html", "myWindow", "width=800,height=500");
+}
+function usingEquivalence(){
+     var myWindow = window.open("../helpSheets/usingEquivalence.html", "myWindow", "width=800,height=500");
+}
+
+function test(){
+    document.getElementById("test").innerHTML = "hello world";
+}
