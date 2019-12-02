a71da754f09b710d62c989c4bb553557397cab5d
clare <clarepritchard@hotmail.com>
2019-12-02 00:17:24 +0000

    Add files via upload

--- public_html/javascript/periodicTable1.js
+++ public_html/javascript/periodicTable1.js
@@ -8,3 +8,19 @@
 function sodium(){
     
 }
+
+function group1Window() {
+    var myWindow = window.open("../html/group1BigImage.html", "myWindow", "width=600,height=600");
+    
+   
+}
+function group1Window2() {
+    var myWindow = window.open("../group1BigImage.html", "myWindow", "width=600,height=600");
+    
+   
+}
+function group7Window2() {
+    var myWindow = window.open("../Group7BigImage1.html", "myWindow", "width=600,height=600");
+    
+   
+}
